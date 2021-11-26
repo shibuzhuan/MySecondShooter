2970441ead5f65eab470dcbbd514d21b09d65a88
shibuzhuan <1065370468@qq.com>
2021-11-26 19:31:40 +0800

    Initial commit

---
+++ Source/MySecondShooter/MySecondShooter.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MySecondShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MySecondShooter, "MySecondShooter" );
+ 
